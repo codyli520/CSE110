1de2f44f0d080c9244438e3a3648fb3591103b00
Cody <codyli520@gmail.com>
2014-06-04 00:20:42 -0700

    confused

--- CSE110 Project/src/Message/AppStarter.java
+++ CSE110 Project/src/Message/AppStarter.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.*;
-
+import javax.swing.SwingUtilities;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,19 +13,17 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class AppStarter extends JFrame implements ActionListener {
 	String command = null;
-	
-	
-	
-    public void begin() throws FileNotFoundException, UnsupportedEncodingException{
-
-	
+	String command2 = null;
+	Object o;
+	String topic;
 	boolean start = true;
 	Scanner scan = new Scanner(System.in);
 	InputStreamReader ISR = new InputStreamReader(System.in);
-    BufferedReader in = new BufferedReader(ISR);
+	BufferedReader in = new BufferedReader(ISR);
 	File file;
 	File dir;
 	User user1 = new User(100,"user1","0");
@@ -37,213 +35,254 @@ public class AppStarter extends JFrame implements ActionListener {
 	String system_username = System.getProperty("user.name");
 	CreateText newtext;
 	WriteText writetext;
-	
-	directoryPath = "/Users/" + system_username + "/Desktop/ChatHistory/";
-	dir = new File(directoryPath);
-	if (!dir.exists()) {
-	      dir.mkdir(); 
-	}
-	
-	filePath = directoryPath + "host-subject";
-	file = new File(filePath);
-	if(!file.exists()){
-		newtext = new CreateText(directoryPath,"subject" , "host");
-		writetext = new WriteText(directoryPath+"host-subject");
-		writetext.writeToText("Subject List:");
-		//newtext = new CreateText(directoryPath,"subject" , "host");
-	}
-	
-	System.out.println("App Started...");
-	System.out.println("Please enter Command: ");
-	
-	while(start){
-		try{
-			if( command == null && in.ready()){
-				command = in.readLine();
-				if( command == null || command.equals("") ){
-					command = null;
-					continue;
+	int topicSent= 0;
+	//	Appstarter app;
+
+
+	public void begin() throws FileNotFoundException, UnsupportedEncodingException{
+
+
+
+
+		directoryPath = "/Users/" + system_username + "/Desktop/ChatHistory/";
+		dir = new File(directoryPath);
+		if (!dir.exists()) {
+			dir.mkdir(); 
+		}
+
+		filePath = directoryPath + "host-subject";
+		file = new File(filePath);
+		if(!file.exists()){
+			newtext = new CreateText(directoryPath,"subject" , "host");
+			writetext = new WriteText(directoryPath+"host-subject");
+			writetext.writeToText("Subject List:");
+			//newtext = new CreateText(directoryPath,"subject" , "host");
+		}
+
+		System.out.println("App Started...");
+		System.out.println("Please enter Command: ");
+
+		while(start){
+			try{
+				if( command == null && in.ready()){
+					command = in.readLine();
+					if( command == null || command.equals("") ){
+						command = null;
+						continue;
+					}
+					else{
+						System.out.println("terminal command " + command);
+					}
 				}
 				else{
-					System.out.println("terminal command " + command);
+					if( command != null )
+					{
+						System.out.println("GUI command " + command);
+					}
+					else{
+						continue;
+					}
 				}
 			}
-			else{
-				if( command != null )
-				{
-					System.out.println("GUI command " + command);
+			catch(IOException e){
+				System.out.println("well");
+				continue;
+			}
+
+			//if command is view
+			if(command.equals("view")){
+
+				fileName = "host-subject";
+				filePath = directoryPath+fileName;
+				file = new File(filePath);   
+
+				try {
+					reader = new BufferedReader(new FileReader(file));
+
+					while ((text = reader.readLine()) != null) {
+						process.append("\n"+text);
+						System.out.println(text);
+					}
+
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						if (reader != null) {
+							reader.close();
+						}
+					} catch (IOException e) {
+					}
 				}
-				else{
-					continue;
+
+			}
+			else if(command.equals("viewsubject")){
+
+				System.out.println("Please enter the subject name: ");
+				String subject = scan.next();
+				fileName = subject;
+				filePath = directoryPath + subject;
+				file = new File(filePath);   
+
+				try {
+					reader = new BufferedReader(new FileReader(file));
+
+					while ((text = reader.readLine()) != null) {
+						System.out.println(text);
+					}
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						if (reader != null) {
+							reader.close();
+						}
+					} catch (IOException e) {
+					}
 				}
+
 			}
-		}
-		catch(IOException e){
-			System.out.println("well");
-			continue;
-		}
+			else if(command.equals("viewpost")){
 
-	    //if command is view
-	    if(command.equals("view")){
-	    	System.out.println("modeview");
-		fileName = "host-subject";
-		filePath = directoryPath+fileName;
-		file = new File(filePath);   
+				System.out.println("Please enter the topic name: ");
+				String topicname = scan.next();
+				fileName = topicname;
+				filePath = directoryPath + fileName;
+				if(topicname != null)
+					user1.receiveMessage(topicname);
+				else
+					System.out.println("topic name cannot be empty");
 
-		try {
-		    reader = new BufferedReader(new FileReader(file));
-
-		    while ((text = reader.readLine()) != null) {
-			System.out.println(text);
-		    }
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		} finally {
-		    try {
-			if (reader != null) {
-			    reader.close();
 			}
-		    } catch (IOException e) {
-		    }
-		}
+			else if(command.equals("posttext") ){
+				process.append("\n\nPlease enter the topic name");
+				System.out.println("Please enter the topic name:");
 
-	    }
-	    else if(command.equals("viewsubject")){
-		
-		System.out.println("Please enter the subject name: ");
-		String subject = scan.next();
-		fileName = subject;
-		filePath = directoryPath + subject;
-		file = new File(filePath);   
+				topic= input.getText() ;
+				process.append("\n"+topic);
 
-		try {
-		    reader = new BufferedReader(new FileReader(file));
+				//input.addActionListener(this);
 
-		    while ((text = reader.readLine()) != null) {
-			System.out.println(text);
-		    }
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		} finally {
-		    try {
-			if (reader != null) {
-			    reader.close();
+				while(topic==null || topic.equals("")){
+
+					if((topic = scan.nextLine())== null){
+						process.setText("Topic cannot be null");
+						System.out.println("Topic cannot be null");
+					}
+
+				}
+				
+				filePath = directoryPath + user1.getUsername()+"-"+topic;
+				writetext = new WriteText(directoryPath+"host-subject");
+				writetext.writeToText(user1.getUsername()+"-"+topic);
+				newtext = new CreateText(directoryPath, topic, user1.getUsername());
+				//topicSent = 1;
+
+				process.append("\n\nPlease Enter the Post Content:");
+				System.out.println("Please Enter the Post Content:");
+				input.setText("");
+			//}
+
+			//else if(command.equals("sent")){
+				//topic= input.getText() ;
+				//newtext = new CreateText(directoryPath, topic, user1.getUsername());
+				newtext.writeToFile(topic);
+
+				if(topic != null){
+					user1.publishMessage(user1.getUsername()+"-"+topic, filePath);
+				}
+				//newtext = new CreateText(directoryPath, topic, user1.getUsername());
 			}
-		    } catch (IOException e) {
-		    }
-		}
-		
-	    }
-	    else if(command.equals("viewpost")){
-		
-		System.out.println("Please enter the topic name: ");
-		String topicname = scan.next();
-		fileName = topicname;
-		filePath = directoryPath + fileName;
-		if(topicname != null)
-		    user1.receiveMessage(topicname);
-		else
-		    System.out.println("topic name cannot be empty");
-		
-	    }
-	    else if(command.equals("posttext")){
-		
-		System.out.println("Please enter the topic name:");
-		String topic = scan.nextLine();
-		while(topic==null || topic.equals("")){
-		    topic = scan.nextLine();
-		    if(topic == null){
-			System.out.println("Topic cannot be null");
-		    }
-		    
-		}
-		System.out.println("Please Enter the Post Content:");
-		filePath = directoryPath + user1.getUsername()+"-"+topic;
-		writetext = new WriteText(directoryPath+"host-subject");
-		writetext.writeToText(user1.getUsername()+"-"+topic);
-		newtext = new CreateText(directoryPath, topic, user1.getUsername());
-		newtext.writeToFile();
-		if(topic != null){
-		    user1.publishMessage(user1.getUsername()+"-"+topic, filePath);
-		}
-		//newtext = new CreateText(directoryPath, topic, user1.getUsername());
-	    }
 
-	    else if(command.equals("quitapp")){
-	    	break;
-	    }
-	    command = null;
-	}	
-	
-	scan.close();
-	System.out.println("app quitted");
-    }
+
+			else if(command.equals("quitapp")){
+				break;
+			}
+			command = null;
+		}	
+
+		scan.close();
+		System.out.println("app quitted");
+	}
 
 
 
-	
+
 	private JLabel label;
-	
-	//private JTextField command;
-	
-	
+
+	private JTextField input;
+
+
 	//private JTextField tfServer, tfPort;
-	
-	private JButton view, viewsubject,viewpost,posttext,quitapp;
-	
+
+	private JButton view, viewsubject,viewpost,posttext,quitapp,send;
+
 	private JTextArea process;
-	
+
 	private boolean connected;
 	//private Sender Sender;
 	private int defaultPort;
 
-	
+
 	AppStarter(){
-		
+		input = new JTextField(10);
 		view = new JButton("View");
 		viewsubject = new JButton("View Subject");
 		viewpost = new JButton("View Post");
 		posttext = new JButton("Post");
 		quitapp = new JButton("quit");
+		send = new JButton("Send");
+
+
 		view.addActionListener(this);
 		viewsubject.addActionListener(this);
 		viewpost.addActionListener(this);
 		posttext.addActionListener(this);
 		quitapp.addActionListener(this);
-		JPanel northPanel = new JPanel ();
-		northPanel.add(view);
-		northPanel.add(viewsubject);
-		northPanel.add(viewpost);
-		northPanel.add(posttext);
-		northPanel.add(quitapp);
-		
+		send.addActionListener(this);
+
+
+		JPanel northPanel = new JPanel (new GridLayout(2,1));
+		JPanel bot = new JPanel (new GridLayout(1,2));
+		JPanel bot2 = new JPanel (new GridLayout(1,5));
+		bot.add(input);
+		bot.add(send);
+		northPanel.add(bot);
+		bot2.add(view);
+		bot2.add(viewsubject);
+		bot2.add(viewpost);
+		bot2.add(posttext);
+		bot2.add(quitapp);
+
+		northPanel.add(bot2);
 		this.add(northPanel,BorderLayout.SOUTH);
-		
+
 		process= new JTextArea("App Started...\nPlease select Command : \n", 40, 40);
 		JPanel centerPanel = new JPanel(new GridLayout(1,1));
 		centerPanel.add(new JScrollPane(process));
 		process.setEditable(false);
 		add(centerPanel, BorderLayout.CENTER);
-		
+
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(600, 600);
 		setVisible(true);
-		
+
 	}
-	
+
 	void showText(String str) {
 		process.append(str);
 		process.setCaretPosition(process.getText().length() - 1);
 	}
 	public void actionPerformed(ActionEvent e) {
-		Object o = e.getSource();
+		o = e.getSource();
 		if(o==view){
 			command = "view";
-			process.setText("");
+
+
 		}
 		else if (o== viewsubject){
 			command = "viewsubject";
@@ -252,11 +291,22 @@ public class AppStarter extends JFrame implements ActionListener {
 			command = "viewpost";
 		}
 		else if (o == posttext){
+
 			command = "posttext";
+
 		}
 		else if (o == quitapp){
 			command = "quitapp";
-			
+			System.exit(0);
+		}
+		else if( o ==  send){
+			if(topicSent == 0)
+				command = "send";
+			else if(topicSent == 1)
+				command = "sent";
+			else if(topicSent == 2)
+				command = "sent";
+			//topic = input.getText().trim();
 		}
 	}
 
--- CSE110 Project/src/Message/CreateText.java
+++ CSE110 Project/src/Message/CreateText.java
@@ -4,31 +4,38 @@ import java.io.*;
 import java.util.Scanner;
 
 public class CreateText {
-    
-    File file;
-    String filepath;
-    PrintWriter writer;
-    Scanner scan;
-    String input;
-    String user;
-    String subject;
-    
-    
-    public CreateText(String dirpath, String subject, String user){
-	this.filepath = dirpath+ user +"-"+subject;
-	this.user = user;
-	this.subject = subject;
-    }
-    
-    public void writeToFile() throws FileNotFoundException, UnsupportedEncodingException{
-	writer = new PrintWriter( filepath, "UTF-8");
-	writer.println(user + ":" + subject);
-	scan = new Scanner(System.in);
-	input = scan.nextLine();
-	while(!input.equals("postend")){
-	    writer.print(input);
-	    input = scan.nextLine();
+
+	File file;
+	String filepath;
+	PrintWriter writer;
+	Scanner scan;
+	String input;
+	String user;
+	String subject;
+
+
+	public CreateText(String dirpath, String subject, String user){
+		this.filepath = dirpath+ user +"-"+subject;
+		this.user = user;
+		this.subject = subject;
 	}
-	writer.close();
-    }
+
+	public void writeToFile(String input) throws FileNotFoundException, UnsupportedEncodingException{
+
+		
+		if(!input.equals("postend")){
+			writer = new PrintWriter( filepath, "UTF-8");
+			writer.println(user + ":" + subject);
+			scan = new Scanner(System.in);
+			input = scan.nextLine();
+			while(!input.equals("postend")){
+				writer.print(input);
+				input = scan.nextLine();
+			}
+		}
+		else{
+			//writer.close();
+		}
+		
+	}
 }
